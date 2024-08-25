1a27019f71b983bea09a72a815ede42bdf4b5cd3
atul <atulsm@gmail.com>
2024-08-25 14:07:37 +0530

    adding maps

--- hello/helloworld.go
+++ hello/helloworld.go
@@ -3,6 +3,7 @@ package main
 import (
 	"experiments/greetings"
 	"fmt"
+	"log"
 
 	"rsc.io/quote"
 )
@@ -11,6 +12,17 @@ func main() {
 	fmt.Println("Hello go")
 	fmt.Println(quote.Go())
 	fmt.Println(greetings.Hello("Atul"))
-	fmt.Println(greetings.Hello(""))
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 
 }
